d3a180a82bed1b7f3fe2b328f78acba59478948d
mtrbls <mourad.trabelsi@digital.justice.gov.uk>
2021-01-25 07:27:56 +0000

    fmt

--- examples/flow_logs.tf
+++ examples/flow_logs.tf
@@ -1,8 +1,10 @@
 
-module "flow_logs"{
-      source= "github.com/ministryofjustice/cloud-platform-terraform-flow-logs?ref=1.2"
-      # vpc_id="my-vpc" 
-      #  OR subnet_id=
-      #  OR eni_id=
-      # traffic_type="ALL"
-  }
+module "flow_logs" {
+  source     = "github.com/ministryofjustice/cloud-platform-terraform-flow-logs?ref=1.3"
+
+  #  is_enabled= true
+  #  vpc_id = "vpc-0e316fa4e1699f39d"
+  #  OR subnet_id=
+  #  OR eni_id=
+  #  traffic_type="ALL"
+}
