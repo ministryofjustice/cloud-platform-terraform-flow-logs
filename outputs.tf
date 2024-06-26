e9c1b3338c10b99592187e683f3cad8a00e67c88
Ky <ky.truong@digital.justice.gov.uk>
2024-06-26 15:56:06 +0100

    :wrench:add condition to output only if enabled

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_arn" {
   description = "s3 bucket arn"
-  value = length(aws_s3_bucket.flow_logs) > 0 ? aws_s3_bucket.flow_logs[0].arn : null
+  value = var.is_enabled ? aws_s3_bucket.flow_logs[0].arn : null
 }
