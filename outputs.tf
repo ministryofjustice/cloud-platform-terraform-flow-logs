cdcbb344f64919171f4cfb257a82affe66edc194
Ky <ky.truong@digital.justice.gov.uk>
2024-06-26 14:12:27 +0100

    :bug:add condition to output as resource doesn't exist in live-2

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_arn" {
   description = "s3 bucket arn"
-  value = aws_s3_bucket.flow_logs[0].arn
+  value = length(aws_s3_bucket.flow_logs) > 0 ? aws_s3_bucket.flow_logs[0].arn : null
 }
