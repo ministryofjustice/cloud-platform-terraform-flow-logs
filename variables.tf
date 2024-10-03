2fe0563bfca624bd8d473341948eaff29f6c8a50
Ky <ky.truong@digital.justice.gov.uk>
2024-10-03 15:17:43 +0100

    :wrench:additional flowlog fields required for Cortex XSIAM

--- variables.tf
+++ variables.tf
@@ -28,3 +28,8 @@ variable "is_enabled" {
   description = "switch to enable/disable the module, defaults to false"
   default = false
 }
+
+variable "log_format" {
+  description = "Fields to include in the flow log record"
+  default = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${az-id} $${flow-direction} $${instance-id} $${pkt-srcaddr} $${pkt-dstaddr} $${region} $${sublocation-id} $${sublocation-type} $${subnet-id} $${tcp-flags} $${type} $${vpc-id}"
+}
